83f1a8ee3c79152289568da99158f04b4a5051d5
Katie Belanger <belangerkat@gmail.com>
2020-02-12 21:58:46 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -13,24 +13,25 @@
 #     }
 #   }
 # }
-# 
+#
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+#
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+#
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
 # }
 
 def naming_system
+  {
   # Remember:
   #  Kingdom
   #  Phylum
@@ -41,5 +42,28 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
+
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class",
+      sub_category: {
+        label: "Order",
+        sub_category: {
+          label: "Family",
+          sub_category: {
+            label: "Genus",
+            sub_category: {
+              label: "Species",
+              sub_category: nil
+            }
+          }
+        }
+      }
+
+    }
+  }
+}
 
 end
